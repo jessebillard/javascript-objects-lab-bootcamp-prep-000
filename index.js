cf4584d7719da2a0a40a7b74d04f24b871c99064
Jesse Billard <jessebillard@mac.com>
2018-02-15 21:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes.prop2 = 2;
+  recipes.prop = 1;
   return recipes
 }
 /* good */
