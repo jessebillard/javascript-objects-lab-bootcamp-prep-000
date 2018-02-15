61568e8b56fe24204bf5448208c8f3ad8ea244b5
Jesse Billard <jessebillard@mac.com>
2018-02-15 22:08:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var recipes = {
-  prop: 1
+  
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key] : value});
+  return Object.assign({}, recipes, {[prop] : 1});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  recipes[prop2] = 2
+  return recipes
 }
 /* good */
 
