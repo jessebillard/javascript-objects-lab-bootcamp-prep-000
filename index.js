0a871ce6a6fda3823ffd05fa2910ee9752ff94b1
Jesse Billard <jessebillard@mac.com>
2018-02-15 22:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var rec = Object.assign({}, object, {[key] : value});
-  return rec
+  return rec;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  let newRec = Object.assign({}, recipes)
-  delete newRec[key]
-  return newRec
+  let newRec = Object.assign({}, recipes);
+  delete newRec[key];
+  return newRec;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
+  delete object.key;
+  return object;
 }
 
 
