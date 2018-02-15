8ad32336106f30e23cff515406a63413c7b1d047
Jesse Billard <jessebillard@mac.com>
2018-02-15 21:48:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, {['prop'] : 1});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+/* good */
+
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
+
+
+
+
+
