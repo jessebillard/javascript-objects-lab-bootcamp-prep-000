a68a790e17e98321730c281703c422179c19a828
Jesse Billard <jessebillard@mac.com>
2018-02-15 22:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key] : value});
 }
 
+
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
 
+console.log(destructivelyUpdateObjectWithKeyAndValue(testObj, 'age', 32))
 
 
 function deleteFromObjectByKey(object, key) {
